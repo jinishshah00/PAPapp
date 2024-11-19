c9e0b985ba7abf0caba3dbe4f5c84b9ea75eb3ae
Tishyaketu <dtishyaketu@gmail.com>
2024-11-18 16:02:38 -0800

    adding Adoption Form functionality

---
+++ backend/controllers/formController.js
@@ -0,0 +1,7 @@
+// create form, by initiated by adopter
+// edit the form by adopter
+// accept form by shelterOwner: formAdoption Model status will change to approve
+// decline form totally: formAdoption Model status will change to decline
+// decline for the date: : formAdoption Model status will change to temporary decline
+// if status=temp, while status becomes approve/decline, keep on asking adopter for new dates
+// is anything else required, ask chatgpt, any logical loophloes, if no problem just write the code
